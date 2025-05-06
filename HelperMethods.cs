188ee6c8d5fe589e0a3bee1de9d03a0380f055d1
alexscespedes <alexandersenc99@gmail.com>
2025-05-06 18:57:14 -0400

    Update CheckIfEmpty method type to bool

--- HelperMethods.cs
+++ HelperMethods.cs
@@ -1,11 +1,12 @@
 namespace ToDoList;
 
 public class HelperMethods {
-    public void CheckIfEmpty(List<Task> tasks) {
+    public bool CheckIfEmpty(List<Task> tasks) {
         if (tasks.Count == 0)
         {
-            Console.WriteLine("Task not found");
-            return;
+            Console.WriteLine("Task not found.");
+            return true;
         }
+        return false;
     }
 }
